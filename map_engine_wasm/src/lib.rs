fc1f893773c6e3c23bcec8079fd58b1d36b0946b
Sho Nakatani <lay.sakura@gmail.com>
2023-12-08 06:23:37 +0900

    chore: add bevy_ecs and its example code

--- map_engine_wasm/src/lib.rs
+++ map_engine_wasm/src/lib.rs
@@ -22,4 +22,45 @@ pub fn start() {
         "Hello world!!!!!! {}",
         &map_engine_core::add(2, 3)
     ));
+
+    // Create a new empty World to hold our Entities and Components
+    let mut world = World::new();
+
+    // Spawn an entity with Position and Velocity components
+    world.spawn((Position { x: 0.0, y: 0.0 }, Velocity { x: 1.0, y: 0.0 }));
+
+    // Create a new Schedule, which defines an execution strategy for Systems
+    let mut schedule = Schedule::default();
+
+    // Add our system to the schedule
+    schedule.add_systems(movement);
+
+    // Run the schedule once. If your app has a "loop", you would run this once per loop
+    schedule.run(&mut world);
+}
+
+use bevy_ecs::prelude::*;
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+#[derive(Component)]
+struct Velocity {
+    x: f32,
+    y: f32,
+}
+
+// This system moves each entity with a Position and Velocity component
+fn movement(mut query: Query<(&mut Position, &Velocity)>) {
+    for (mut position, velocity) in &mut query {
+        position.x += velocity.x;
+        position.y += velocity.y;
+
+        log(&format!(
+            "movement - position ({}, {})",
+            &position.x, &position.y
+        ));
+    }
 }
